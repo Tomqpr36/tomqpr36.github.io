2853c6f0cf807477bbc296dde442f23f48076d45
tom Pitts <Tomqpr36@gmail.com>
2021-05-25 13:31:47 +0100

    Added birthday countdown

--- src/app.js
+++ src/app.js
@@ -6,3 +6,36 @@ function toggleButton() {
 }
 
 hamburgerBtn.addEventListener("click", toggleButton)
+
+// birthday countdown
+const today = new Date()
+const birthday = new Date("05/23/2022")
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+let timerId
+const timeLeft = document.getElementById('time-left')
+
+function countDown() {
+    const today = new Date()
+    const timeSpan = birthday - today
+    if (timeSpan <= -day){
+        timeLeft.innerHTML = "Woo it's been my birthday!"
+        clearInterval(timeID)
+        return
+    }
+    if (timeSpan <= 0) {
+        timeLeft.innerHTML = "It's My Birthday!!!"
+        clearInterval(timerId)
+        return
+    }
+    const days = Math.floor(timeSpan / day )
+    const hours = Math.floor((timeSpan % day) / hour)
+    const minutes = Math.floor((timeSpan % hour) / minute)
+    const seconds = Math.floor((timeSpan % minute) / second)
+    
+    timeLeft.innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`
+}
+
+setInterval(countDown, second)
